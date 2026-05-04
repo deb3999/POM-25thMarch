1804184b0e3ff3ba5c27e054df7d32e8151c67a5
deb3999 <deb3999@hotmail.com>
2026-05-05 02:51:20 +0530

    added if block in VS code

--- testfile5.py
+++ testfile5.py
@@ -1,1 +1,7 @@
-Print("hello this is testfile 5  added in GITHUB BY deb3999")
+print("hello this is testfile 5  added in GITHUB BY deb3999")
+
+if 1>0:
+    print("This is true")
+    
+else:
+    print("This is false")
